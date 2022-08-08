e60ae0db138d0c396e021034bac0dffe3fcba8a0
ELIUS <eliuswanyoro@gmail.com>
2022-08-08 16:53:44 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution in this file!
+let  employee = { 
+    name: "Allan",     
+    streetAddress: "Ruiru,Nairobi"
+};
+
+function  updateEmployeeWithKeyAndValue(employee, key, value){
+    let  newObject = { ...employee };
+  
+    newObject[key] = value;
+  
+    return  newObject ; 
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(newEmployee, key, value){
+
+    employee[key] = value;
+  
+    return employee;
+
+}
+   function  deleteFromEmployeeByKey(employee, key) {
+    let secondEmployee = { ...employee};
+    delete secondEmployee[key]
+    
+      return secondEmployee
+
+   }
+
+   function  destructivelyDeleteFromEmployeeByKey(employee, key){
+    let thirdObject  = {...employee};
+
+    delete  employee[key];
+    
+    return employee
+   }
+  
+   
+   
+
+
+     
